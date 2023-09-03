88a641a50ecf3ffbb72df12b22081fb29bfe3171
Ricardo Faria <ricardo.faria@sky.uk>
2023-09-03 20:37:34 +0100

    change User tablename into Account 2

--- rest/src/test/resources/data.sql
+++ rest/src/test/resources/data.sql
@@ -1,5 +1,5 @@
-insert into "user" ("email","is_active","is_admin","name","password", "id")
-values ('luffy@mugiwara.com',true, true,'Monkey D Luffy','meat123', nextval('user_seq'));
+insert into "account" ("email","is_active","is_admin","name","password", "id")
+values ('luffy@mugiwara.com',true, true,'Monkey D Luffy','meat123', nextval('account_seq'));
 
-insert into "user" ("email","is_active","is_admin","name","password", "id")
-values ('zoro@mugiwara.com',false, false,'roronoa zoro','meat123', nextval('user_seq'));
+insert into "account" ("email","is_active","is_admin","name","password", "id")
+values ('zoro@mugiwara.com',false, false,'roronoa zoro','meat123', nextval('account_seq'));
