8805323fd61a9163e3c86de9598de31d081686ea
DrKimochi <rikkardo.faria@gmail.com>
2023-10-31 14:45:27 +0000

    productController WIP 7

--- rest/src/test/java/drk/shopamos/rest/controller/ControllerTest.java
+++ rest/src/test/java/drk/shopamos/rest/controller/ControllerTest.java
@@ -16,9 +16,9 @@ import drk.shopamos.rest.controller.assertion.ErrorResponseAssert;
 import drk.shopamos.rest.model.entity.Account;
 import drk.shopamos.rest.service.AccountService;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -50,7 +50,12 @@ public abstract class ControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    public MockMvcHandler getMvc() {
+    @BeforeEach
+    protected void setup() {
+        SecurityContextHolder.clearContext();
+    }
+
+    protected MockMvcHandler getMvc() {
         return new MockMvcHandler(mockMvc, objectMapper);
     }
 
@@ -71,7 +76,6 @@ public abstract class ControllerTest {
         account.setId(accountId);
         account.setEmail(LUFFY_EMAIL);
         account.setAdmin(isAdmin);
-        SecurityContextHolder.clearContext();
         when(accountService.loadUserByUsername(LUFFY_EMAIL)).thenReturn(account);
         return jwtTokenHelper.generateToken(account);
     }
