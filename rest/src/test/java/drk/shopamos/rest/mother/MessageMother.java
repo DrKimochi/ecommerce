a728033dba75a583aa534fe82db2a93affd019b3
DrKimochi <rikkardo.faria@gmail.com>
2023-11-12 00:59:06 +0000

    orderController WIP 4

--- rest/src/test/java/drk/shopamos/rest/mother/MessageMother.java
+++ rest/src/test/java/drk/shopamos/rest/mother/MessageMother.java
@@ -1,7 +1,6 @@
 package drk.shopamos.rest.mother;
 
 public class MessageMother {
-
     public static final String MSG_FORM_FIELD = "error.form.field";
     public static final String MSG_FIELD_EMPTY = "error.form.field.empty";
     public static final String MSG_FIELD_EMAIL = "error.form.field.email";
@@ -9,15 +8,27 @@ public class MessageMother {
     public static final String MSG_FIELD_PASSWORD = "error.form.field.password";
     public static final String MSG_FIELD_CATEGORY = "error.form.field.category";
     public static final String MSG_FIELD_MAX_LENGTH = "error.form.field.maxlength";
-    public static final String MSG_BODY_UNREADABLE = "error.request.body.unreadable";
+
     public static final String MSG_NOT_FOUND_ID = "error.service.entity.notfound.id";
     public static final String MSG_NOT_FOUND_CATEGORY = "error.service.entity.notfound.category";
+    public static final String MSG_NOT_FOUND_USER = "error.service.entity.notfound.username";
     public static final String MSG_EXISTS_EMAIL = "error.service.entity.exists.email";
     public static final String MSG_EXISTS_CATEGORY = "error.service.entity.exists.category";
-    public static final String MSG_PARAM_WRONG_TYPE = "error.request.param.wrongtype";
+
+    public static final String MSG_ORDER_STATUS_REQUIRED =
+            "error.service.entity.required.orderstatus";
+    public static final String MSG_BODY_UNREADABLE = "error.request.body.unreadable";
     public static final String MSG_CANNOT_PROMOTE = "error.forbidden.entity.cannot.promote";
     public static final String MSG_CANNOT_TARGET_OTHERS =
             "error.forbidden.entity.cannot.target.others";
 
-    public static final String RETURNED_MSG = "Message returned from exception";
+    public static final String MSG_CANNOT_GET_INACTIVE =
+            "error.forbidden.product.cannot.get.inactive";
+    public static final String MSG_PARAM_WRONG_TYPE = "error.request.param.wrongtype";
+
+    public static final String MSG_ORDER_NO_ITEMS = "error.service.entity.order.items.notfound";
+    public static final String MSG_ORDER_ITEM_QTY_GT0 =
+            "error.service.entity.order.items.quantity.gt0";
+    public static final String MSG_USERNAME_REQUIRED = "error.service.entity.required.user";
+    public static final String RETURNED_MSG = "the returned exception message";
 }
